1e2f7b24b31f3241b38cdb1b594ce9209bc391fc
Nirbhay Kumar Mandelia <nmandelia@sapient.com>
2017-09-06 17:09:50 +0530

    storybook changes

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,33 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { ThemeProvider } from 'theming';
+import theme from '../theme/index';
+import { withInfo } from '@storybook/addon-info';
+
+// addon-info
+//  setDefaults({
+//      header: false, // Toggles display of header with component name and description
+//      inline: true, // Displays info inline vs click button to view
+//      source: true, // Displays the source of story Component
+//      propTables: [/* Components used in story */], // displays Prop Tables with this components
+//      propTablesExclude: [], // Exclude Components from being shoun in Prop Tables section
+//      styles: {}, // Overrides styles of addon
+//      marksyConf: {}, // Overrides components used to display markdown. Warning! This option's name will be likely deprecated in favor to "components" with the same API in 3.3 release. Follow this PR #1501 for details
+//      maxPropsIntoLine: 1, // Max props to display per line in source code
+//      maxPropObjectKeys: 10,
+//      maxPropArrayLength: 10,
+//      maxPropStringLength: 100,
+//  });
+
+addDecorator((story, context) => withInfo('Component Info')(story)(context));
+
+addDecorator(story => (
+    <ThemeProvider theme={theme}>
+        {story()}
+    </ThemeProvider>
+));
 
 function loadStories() {
-  require('../stories/index.js');
-  // You can require as many stories as you need.
+    require('../stories/index.js');
 }
 
 configure(loadStories, module);
--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { ThemeProvider } from 'theming';
 import {
   Alert,
   Avatar,
@@ -35,35 +34,113 @@ import {
   ToggleButton,
   Tooltip,
   Truncate,
-  Typing,
-  Tabs,
+  Typing
 } from '../';
 
-storiesOf('Alert', module)
+/****************************
+ * Atomic
+ ****************************/
+
+storiesOf('Atomic/Alert', module)
   .add('with info', () => (
-    <ThemeProvider theme={theme}>
-      <Alert type="info">This is info!!</Alert>
-    </ThemeProvider>
+    <Alert type="info">This is info!!</Alert>
   ))
   .add('with warning', () => (
-    <ThemeProvider theme={theme}>
-      <Alert type="warning">This is warning!!</Alert>
-    </ThemeProvider>
+    <Alert type="warning">This is warning!!</Alert>
   ))
   .add('with success', () => (
-    <ThemeProvider theme={theme}>
-      <Alert type="success">This is success!!</Alert>
-    </ThemeProvider>
+    <Alert type="success">This is success!!</Alert>
   ))
   .add('with error', () => (
-    <ThemeProvider theme={theme}>
-      <Alert type="error">This is error!!</Alert>
-    </ThemeProvider>
+    <Alert type="error">This is error!!</Alert>
   ));
 
-storiesOf('Avatar', module)
+storiesOf('Atomic/Avatar', module)
   .add('default', () => (
-    <ThemeProvider theme={theme}>
-      <Avatar />
-    </ThemeProvider>
+    <Avatar />
   ));
+
+storiesOf('Atomic/BackgroundImage', module)
+  .add('default', () => (
+    <BackgroundImage />
+  ));
+
+storiesOf('Atomic/Banner', module)
+  .add('default', () => (
+    <Banner />
+  ));
+
+storiesOf('Atomic/BlockQuote', module)
+  .add('default', () => (
+    <BlockQuote>Blockquote tag specifies a section that is quoted from another source.</BlockQuote>
+  ));
+
+storiesOf('Atomic/Bounce', module)
+  .add('default', () => (
+    <Bounce />
+  ));
+
+storiesOf('Atomic/Button', module)
+  .add('default', () => (
+    <Button>Button</Button>
+  ))
+  .add('disabled', () => (
+    <Button disabled>Button</Button>
+  ));
+
+storiesOf('Atomic/CardGrid', module)
+  .add('default', () => (
+    <CardGrid />
+  ));
+
+/****************************
+ * Navigation
+ ****************************/
+
+storiesOf('Navigation/Navbar', module)
+  .add('default', () => (
+    <Navbar />
+  ));
+
+storiesOf('Navigation/Pagination', module)
+  .add('default', () => (
+    <Pagination />
+  ));
+
+
+/****************************
+ * Forms
+ ****************************/
+
+
+/****************************
+ * Loaders
+ ****************************/
+
+storiesOf('Loaders/Loader', module)
+  .add('default', () => (
+    <Loader />
+  ));
+storiesOf('Loaders/ProgressBar', module)
+  .add('default', () => (
+    <ProgressBar />
+  ));
+
+storiesOf('Loaders/ProgressCircle', module)
+  .add('default', () => (
+    <ProgressCircle />
+  ));
+
+/****************************
+ * Charts
+ ****************************/
+
+storiesOf('Charts/BarChart', module)
+.add('default', () => (
+  <BarChart />
+));
+
+// storiesOf('Tabs', module)
+//   .add('default', () => (
+//     <Tabs onClick={() => { }} />
+//   ));
